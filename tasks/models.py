f57a67a489d7dd7862ce5b9df18f0fdcaf3ab212
Kauê <kaue_figueiredo.lp@hotmail.com>
2026-07-10 18:38:53 -0300

    feat: add Task model and initial migration

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+
+    title = models.CharField(max_length=120)
+
+    description = models.TextField(
+        blank=True,
+        max_length=500,
+    )
+
+    completed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
